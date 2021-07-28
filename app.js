fe4e8708f123c07d6bad5a9eb99fc7786c579359
Aryanand Arnav <aryanandarnav13@gmail.com>
2021-07-28 21:29:41 +0530

    refactor : js codes updated

--- app.js
+++ app.js
@@ -9,21 +9,27 @@ const result = document.querySelector(".result");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  let dateSum=0;
+  const array = bday.value.split("-"); // [dd mm yyyy]
+  console.log(array.length);
+  for(let i=0;i<array.length;i++)
+  { console.log(array[i]);
+    dateSum+=parseInt(array[i]);
+  }
+  console.log(dateSum);
+  
 
-  const dateSum = bday.value.split("-").reduce((a, c) => {
-    return Number(a) + Number(c);
-  }, 0);
   const luckyNumber = Number(luckyNum.value);
-
+  console.log(dateSum/luckyNumber);
+  console.log(dateSum % luckyNumber);
   const areYouLucky = dateSum % luckyNumber === 0 ? true : false;
 
-  section.classList.add("lucky-section");
 
   if (areYouLucky) {
-    result.textContent = "Congratulations, You are a lucky person.";
+    result.innerText = "Congratulations, You are a lucky person.";
  
   } else {
-    result.textContent = `You missed by ${
+    result.innerText = `You missed by ${
       dateSum % luckyNumber
     } days from being lucky.`;
   }
