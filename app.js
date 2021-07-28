6f4ca28639dc0ec51def0b824a5706082eeae910
Aryanand Arnav <aryanandarnav13@gmail.com>
2021-07-28 18:47:58 +0530

    refactor : added js

---
+++ app.js
@@ -0,0 +1,32 @@
+const luckyNum = document.querySelector("#lucky-num");
+const bday = document.querySelector("#bday");
+const form = document.querySelector(".form");
+const section = document.querySelector("section");
+
+const result = document.querySelector(".result");
+
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const dateSum = bday.value.split("-").reduce((a, c) => {
+    return Number(a) + Number(c);
+  }, 0);
+  const luckyNumber = Number(luckyNum.value);
+
+  const areYouLucky = dateSum % luckyNumber === 0 ? true : false;
+  luckyNum.value = "";
+  bday.value = "";
+
+  section.classList.add("lucky-section");
+
+  if (areYouLucky) {
+    result.textContent = "Congratulations, You are a lucky person.";
+ 
+  } else {
+    result.textContent = `You missed by ${
+      dateSum % luckyNumber
+    } days from being lucky.`;
+  }
+});
